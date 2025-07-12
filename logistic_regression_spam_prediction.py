7d6a38494b60c9d84162701f607e1b09a1d32378
TAN JIN YUAN <93397040+KTYJ@users.noreply.github.com>
2025-07-12 19:24:52 +0800

    Added driver program

---
+++ logistic_regression_spam_prediction.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from joblib import load
+import numpy as np
+
+#load le model
+lr = load('logistic_regression_model.joblib')
+cv = load('countvectorizer.joblib')
+
+# Create a simple user input
+#user_input = st.number_input('Enter house size:', min_value=100, max_value=10000, step=50)    <-- This is just example, ignore yea
+user_input = st.text_input("Enter any sentence... :")
+
+
+# Predict the house price
+if st.button('Predict Spam/Ham'):
+    Snew = cv.transform(user_input)
+    result = lr.predict(user_input)
+    st.write(f"The predicted sentiment is: {result[0]}")
